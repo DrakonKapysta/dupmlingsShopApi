a54c2dadd5510ef2d66a7afcf6b1c54e6afced8d
Oleg <matyiokin2002@gmail.com>
2023-09-24 13:42:01 +0300

    changed port for deployment

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import * as process from 'process';
 
 const start = async () => {
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 3000;
   try {
     const app = await NestFactory.create(AppModule);
     await app.listen(PORT, () => console.log('Server started on port ' + PORT));
